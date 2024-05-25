bace03cee03fb94144c29eac7d3e77ed4d5ed0c1
wahei628 <wahei628@gmail.com>
2024-05-25 16:30:06 +0900

    本番環境db確認用view作成

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path, notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:title, :description, :url_slug, :password)
+    end
+end
