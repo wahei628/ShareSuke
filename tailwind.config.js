0d1abb141be42306581f520262932b3e363628ee
Dingu-suke <e2310000rubik@gmail.com>
2024-07-10 12:11:08 +0900

    よみこまれん

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js',
-    './app/javascript/**/**/*.js',
-    './app/javascript/**/*.jsx',
-    './app/javascript/**/**/*.jsx',
+    './app/**/*.{html,erb,rb,js,jsx,ts,tsx}',
+    './config/initializers/**/*.rb',
+    './lib/components/**/*.{rb,erb}',
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [require("daisyui")],
+  darkMode: 'class',
 }
