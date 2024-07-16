9c341189a84225f1c1fef544108446256a4f5812
藤井京樹 <miyaki20030223@icloud.com>
2024-07-16 22:08:08 +0900

    ActionCable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,10 @@ class CommentsController < ApplicationController
     @comment.user = User.find(params[:user_id])
 
     if @comment.save
-      redirect_to event_path(params[:event_url_slug]), notice: "Comment was successfully created."
+      rendered_comment = render_to_string(partial: 'comments/comment', locals: { comment: @comment })
+      ActionCable.server.broadcast "comment_channel_#{@comment.user.event_id}", { comment: rendered_comment }
     else
-      redirect_to event_path(params[:event_url_slug]), alert: "There was an error creating the comment."
+      redirect_to event_path(event.url_slug), alert: "コメントの投稿に失敗しました"
     end
   end
 
