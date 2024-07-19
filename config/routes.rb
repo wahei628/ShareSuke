eaaa990dff3efb01dbde91e98093936554e93fef
wahei628 <wahei628@gmail.com>
2024-07-19 17:55:35 +0900

    OXΔ作成&表示

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     resources :users, only: %i[create edit update destroy]
     resources :comments, only: [:create]
   end
+  resources :user_schedules, only: [:index, :create]
   get "react_test" => "react_test#index"
 end
