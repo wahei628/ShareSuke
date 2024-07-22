6016cdfe0ee79f515f45123fea92996c8f11c84c
Dingu-suke <e2310000rubik@gmail.com>
2024-07-22 15:34:06 +0900

    [🐛]タイポ修正

--- app/javascript/react/main.jsx
+++ app/javascript/react/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { CalendarForm } from './newCalendar/components/CalendarFrom';
+import { CalendarForm } from './newCalendar/components/CalendarForm';
 import ScheduleTable from "./newSchedule/components/ScheduleTable";
 
 
