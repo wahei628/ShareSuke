850288010b9221b94786b9f90042d5e7ce76d041
yamadahideto <yahideto428@gmail.com>
2024-09-17 12:02:46 +0900

    不要なコードの削除

--- app/javascript/password.js
+++ app/javascript/password.js
@@ -4,10 +4,6 @@ document.addEventListener("turbo:load", () => {
   const copyButton = document.getElementById("password_copy_btn");
   const tooltip = document.getElementById("password_tooltip");
 
-  passwordField.addEventListener("input", function () {
-    passwordDisplay.textContent = passwordField.value;
-  });
-
   copyButton.addEventListener("click", function () {
     navigator.clipboard
       .writeText(passwordField.value)
