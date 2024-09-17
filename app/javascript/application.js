387853865c0249d5ac62efc4af1e10e934649987
yamadahideto <yahideto428@gmail.com>
2024-09-17 11:47:26 +0900

    ページ遷移時にJSが動かない問題を修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "./react/main"
-import "./channels"
-import "./flash_message"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "./react/main";
+import "./channels";
+import "./flash_message";
+import "./password";
+import "./copy";
